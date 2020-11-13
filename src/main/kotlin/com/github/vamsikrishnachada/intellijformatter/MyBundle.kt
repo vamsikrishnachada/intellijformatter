b64a02869adcdae75a4738f35f68c373edad369c
GitHub Action <action@github.com>
2020-11-13 22:08:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/vamsikrishnachada/intellijformatter/MyBundle.kt
+++ src/main/kotlin/com/github/vamsikrishnachada/intellijformatter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vamsikrishnachada.intellijformatter
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/vamsikrishnachada/intellijformatter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vamsikrishnachada.intellijformatter.services
+
+import com.github.vamsikrishnachada.intellijformatter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
